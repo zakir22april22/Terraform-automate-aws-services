dcda4d50026f238fd01030484de80e4fc60f973d
zakir <zakir1997khan@gmail.com>
2023-09-03 04:13:12 +0530

    terraform-infrastructure-automate-using-alb

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 "# Terraform-automate-aws-services" 
 
-#step 1:- creted vpc
-#step 2:- 2 public subnet created
-#step 3:- internet-gateway created
-#step 4:- route-table created
-#step 5:- ROUTE-TABLE-ASSOCIATION
-#step 6:- SECURITY-GROUP
-#step 7:- S3-BUCKET
-#step 8:- EC2-INSTANCE CREATE
-#step 9:- USERDATA
-#step 10:- variables
+"#step 1:- creted vpc"
+"#step 2:- 2 public subnet created"
+"#step 3:- internet-gateway created"
+"#step 4:- route-table created"
+"#step 5:- ROUTE-TABLE-ASSOCIATION"
+"#step 6:- SECURITY-GROUP"
+"#step 7:- S3-BUCKET"
+"#step 8:- EC2-INSTANCE CREATE"
+"#step 9:- USERDATA"
+"#step 10:- variables"
+"#step 11:- create load-balancer"
+"#step 12:- Target-group"
+"#step 13:- Target-group-attachment"
+"#step 14:- Lister for alb and target group to communicate each other"
+
--- main.tf
+++ main.tf
@@ -1,91 +1,91 @@
 #VPC CREATE
 resource "aws_vpc" "myvpc" {
-    cidr_block = var.cidr
+  cidr_block = var.cidr
 }
 
-#PUBLIC-SUBNET
+#PUBLIC-SUBNET-1
 resource "aws_subnet" "sub1" {
-    vpc_id = aws_vpc.myvpc.id
-    cidr_block = var.cidr1
-    availability_zone = "us-east-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.cidr1
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
+  tags = {
     Name = "public-subnet1"
   }
 }
 
 resource "aws_subnet" "sub2" {
-    vpc_id = aws_vpc.myvpc.id
-    cidr_block = var.cidr2
-    availability_zone = "us-east-1b"
-    map_public_ip_on_launch = true
-    
-    tags = {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.cidr2
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
     Name = "public-subnet2"
   }
 }
 
 #INTERNET-GATEWAY
 resource "aws_internet_gateway" "myigw" {
-    vpc_id = aws_vpc.myvpc.id
-    tags = {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
     Name = "myigw"
   }
 }
 
 #ROUTE-TABLE
 resource "aws_route_table" "RT" {
-    vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.myvpc.id
 
-    route {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myigw.id
   }
-  
+
 }
 
 #ROUTE-TABLE-ASSOCIATION
 resource "aws_route_table_association" "rts1" {
-    subnet_id = aws_subnet.sub1.id
-    route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
 
-  
+
 }
 resource "aws_route_table_association" "rts2" {
-    subnet_id = aws_subnet.sub2.id
-    route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
 
-  
+
 }
 
 #SECURITY-GROUP
 resource "aws_security_group" "sg" {
-  name        = "web-sg"
-  vpc_id      = aws_vpc.myvpc.id
+  name   = "web-sg"
+  vpc_id = aws_vpc.myvpc.id
 
   ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
   ingress {
-    description      = "ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -95,23 +95,75 @@ resource "aws_security_group" "sg" {
 
 #S3-BUCKET
 resource "aws_s3_bucket" "s3-bucket" {
-    bucket = "05-02-1997-bucket"
+  bucket = "05-02-1997-bucket"
 }
 
 #EC2-INSTANCE CREATE
 resource "aws_instance" "webserver1" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub1.id
-  user_data = base64encode(file(("userdata.sh")))
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file(("userdata.sh")))
 }
 
 resource "aws_instance" "webserver2" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub2.id
-  user_data = base64encode(file(("userdata1.sh")))
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file(("userdata1.sh")))
 }
 
+# create load-balancer
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.sg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+
+}
+
+# Target-group
+resource "aws_lb_target_group" "mytg" {
+  name     = "mytg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+#Target-group-attachment
+resource "aws_alb_target_group_attachment" "myattachment" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+
+}
+resource "aws_alb_target_group_attachment" "myattachment1" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+# Lister for alb and target group to communicate each other
+resource "aws_lb_listener" "mylister" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.mytg.arn
+    type             = "forward"
+  }
+
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+
+}
