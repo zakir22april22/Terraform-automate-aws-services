47ee43b5233552e1f5f90f8e9eae155623f1040b
zakir <zakir1997khan@gmail.com>
2023-09-03 00:59:19 +0530

    first-commit

---
+++ main.tf
@@ -0,0 +1,109 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.cidr1
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+
+    tags = {
+    Name = "public-subnet1"
+  }
+}
+
+resource "aws_subnet" "sub2" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.cidr2
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+    
+    tags = {
+    Name = "public-subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+    Name = "myigw"
+  }
+}
+
+resource "aws_route_table" "RT" {
+    vpc_id = aws_vpc.myvpc.id
+
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+  
+}
+
+resource "aws_route_table_association" "rts1" {
+    subnet_id = aws_subnet.sub1.id
+    route_table_id = aws_route_table.RT.id
+
+  
+}
+resource "aws_route_table_association" "rts2" {
+    subnet_id = aws_subnet.sub2.id
+    route_table_id = aws_route_table.RT.id
+
+  
+}
+
+resource "aws_security_group" "sg" {
+  name        = "web-sg"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+    bucket = "05-02-1997-bucket"
+}
+
+resource "aws_instance" "webserver1" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = aws_subnet.sub1.id
+  user_data = base64encode(file(("userdata.sh")))
+}
+
+resource "aws_instance" "webserver2" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = aws_subnet.sub2.id
+  user_data = base64encode(file(("userdata1.sh")))
+}
+
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "cidr" {
+    default = "10.0.0.0/16"
+}
+variable "cidr1" {
+    default = "10.0.0.0/24"
+}
+variable "cidr2" {
+    default = "10.0.1.0/24"
+}
